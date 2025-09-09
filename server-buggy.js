3acbe5d8b5cf942f754ede2ae4b41a20bc7ee6e5
Roy Abellana <abellana@gmail.com>
2025-09-09 15:32:56 +0800

    update

--- server-buggy.js
+++ server-buggy.js
@@ -80,13 +80,62 @@ app.get('/:room', cors(corsOptionsDelegate), (req, res) => {
     res.render('index', { RoomId: req.params.room });
 });
 
+io.on("connection", (socket) => {    
 
 
-var users = [];
-var roomUsers = [];
+    console.log("test user connected, with id " + socket.id)
 
-io.on('connection', function(socket) {
-    //console.log("user connected, with id " + socket.id)
+    socket.on('newUser', function(data) {
+        let peerid = data.id;
+        let roomID = data.roomID;
+        
+        users.push({
+            'id': socket.id,
+            'peerid': peerid,
+            'userid': data.user.userid,
+            'username': data.user.username,
+            'user_image': data.user.user_image,
+            'nickname': data.user.nickname,
+            'status': data.user.status,
+            'type': data.user.type,
+        });      
+        
+        console.log("new user joined", data);
+
+        socket.join(roomID);
+        socket.to(roomID).broadcast.emit('userJoined', data);
+    
+    });
+    
+    socket.on('disconnect', function() {
+
+        console.log("disconnect", socket.id);
+
+        for (var i in users) {
+            if (users[i].id === socket.id) {
+
+               console.log("disconnected user :", users[i])
+
+                io.emit('WEBRTC_USER_LEAVE_SESSION', users[i]);
+                delete users[i];
+                break;
+            }
+        }      
+
+        users = users.filter(function(element) {
+            return element !== undefined;
+        });
+
+        update_webrtc_user_list();        
+    });    
+    
+
+
+    function update_webrtc_user_list() {
+        io.emit('update_webrt_user_list', users);
+    }
+
+
 
 
     /*****************************************/
@@ -129,32 +178,65 @@ io.on('connection', function(socket) {
         //io.sockets.emit("PREV_AUDIO_FILE", data);
     });
 
+    socket.on('changeMedia', (data) => {
 
+    	console.log("user change media ");    	
+          
+        let id = data.id;
+        let roomID = data.roomID;
+        let user = data.user;
+        let videoStream = data.videoStream;
+            	
+    	socket.join(roomID);    		
+	    socket.to(roomID).broadcast.emit('mediaChanged', data);    
+    
+    });
+    
+    socket.on("userShare", (room, videoStream) => {    
+    	console.log("user shared");    	
+    	socket.join(room);    		
+	    socket.to(room).broadcast.emit('userShared', videoStream);
+    });
+
+    
     /*****************************************/
     /*  MEMBER ALL PAGE CALL TO ACTIONS
     /*****************************************/
 
     socket.on("CALL_USER", (data) => {
+
+        console.log("CALL_USER", data.user.nickname);
+
         //io.to('' + data.channelid + '').emit("CALL_USER", data);
         io.sockets.emit("CALL_USER", data);
     });
 
     socket.on("CALL_USER_PINGBACK", (data) => {
+
+        console.log("CALL_USER_PINGBACK", data);
+
         io.to('' + data.channelid + '').emit("CALL_USER_PINGBACK", data);
     });
 
 
     socket.on("ACCEPT_CALL", (data) => {
+
+        console.log("ACCEPT_CALL", data);
+
         io.sockets.emit("ACCEPT_CALL", data);
+        
     });
 
 
     socket.on("DROP_CALL", (data) => {
+
+         console.log("ACCEDROP_CALLPT_CALL", data);
+
         io.sockets.emit("DROP_CALL", data);
     });
 
 
-    /*****************************************/
+ /*****************************************/
     /*  SESSION ACTIONS
     /*****************************************/
 
@@ -178,9 +260,10 @@ io.on('connection', function(socket) {
         io.to('' + data.channelid + '').emit("MEMBER_JOINED", data);
     });
 
-    socket.on("LEAVE_SESSION", (data) => {
-        io.to('' + data.channelid + '').emit("LEAVE_SESSION", data);
-    });
+    //socket.on("LEAVE_CANVAS_SESSION", (data) => {
+        //WHEN USER LEFT THE SESSION, MAKE THE CANVAS USER LEAVE TO
+       //io.to('' + data.channelid + '').emit("LEAVE_CANVAS_SESSION", data);
+    //});
 
 
     socket.on("START_SESSION", (data) => {
@@ -208,6 +291,7 @@ io.on('connection', function(socket) {
     socket.on("STOP_MEMBER_TIMER", (data) => {
         io.to('' + data.channelid + '').emit("STOP_MEMBER_TIMER", data);
     });
+    
 
     /*****************************************/
     /*  CANVAS SERVER
@@ -239,9 +323,8 @@ io.on('connection', function(socket) {
 
     socket.on("TUTOR_SELECTED_NEW_SLIDES", (data) => {
         io.to('' + data.channelid + '').emit("TUTOR_SELECTED_NEW_SLIDES", data);
-    });
-
-
+    });    
+    
 
     /*****************************************/
     /*  ADMIN CHAT SUPPORT MESSENGER 
@@ -249,7 +332,7 @@ io.on('connection', function(socket) {
 
     socket.on("SEND_USER_MESSAGE", function(data) {
 
-        console.log("send user ", data.recipient.username);
+        console.log("SEND_USER_MESSAGE ", data.recipient.username);
 
         //io.sockets.connected[data.id].emit("PRIVATE_MESSAGE", data);
         /*
@@ -273,9 +356,22 @@ io.on('connection', function(socket) {
         //this.handleUserPrivateMsg(data);
     });
 
+    socket.on('JOIN_CHANNEL', function(data) {
+        const { channelID } = data;
+    
+        // Join the specified channel room
+        socket.join(channelID);
+    
+        // Optionally, you can emit a confirmation back to the client
+        socket.emit('CHANNEL_JOINED', { channelID });
+    });
+    
     /*Register connected user*/
     socket.on('REGISTER', function(user) {
-        console.log("user connected, with id " + socket.id + " " + user.username)
+
+        console.log("joined", user);
+
+        //console.log("user connected, with id " + socket.id + " " + user.username)
 
         //remove if ever there is same userid
         /*
@@ -295,12 +391,14 @@ io.on('connection', function(socket) {
 
         users.push({
             'id': socket.id,
+            'channelid': user.channelid,
             'userid': user.userid,
             'username': user.username,
             'user_image': user.user_image,
             'nickname': user.nickname,
             'status': user.status,
             'type': user.type,
+            'chat_type': user.chat_type
         });
 
 
@@ -316,13 +414,27 @@ io.on('connection', function(socket) {
 
     //Removing the socket on disconnect
     socket.on('disconnect', function() {
+
         for (var i in users) {
             if (users[i].id === socket.id) {
-                io.emit('LEAVE_SESSION', users[i]);
-                delete users[i];
+                if (users[i].channelid == null) {
+
+                    console.log("user left session",  users[i])
+                    io.emit('LEAVE_SESSION', users[i]);
+
+                } else if (users[i].chat_type == 'canvas' ) {
+
+                    console.log("user left CANVAS session",  users[i])               
+                    io.to('' + users[i].channelid + '').emit("LEAVE_CANVAS_SESSION", users[i]);
+                }                
+                
+                //delete users[i];
+                users.splice(i, 1); // Remove the user from the array
                 break;
             }
         }
+
+        
 
         users = users.filter(function(element) {
             return element !== undefined;
@@ -336,7 +448,17 @@ io.on('connection', function(socket) {
         io.emit('MESSAGE', data)
     });
 
+    // Private message handling
+    socket.on('SEND_PRIVATE_MESSAGE', function(data) {
+
+        console.log("SEND_PRIVATE_MESSAGE", data.recipientID);
+
+        io.to('' + data.recipientID + '').emit("PRIVATE_MESSAGE_SENT", data);
+    });    
+
 });
+
+
 
 server.listen(port, () => {
     console.log("https listening at: " + port);
